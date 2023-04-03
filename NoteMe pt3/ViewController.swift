148b28a681e06b56e0541fd2d5ceec8bb01f0a92
Aleksandr Kh <akumasanta@gmail.com>
2023-04-03 10:50:21 +0600

    Initial Commit

---
+++ NoteMe pt3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteMe pt3
+//
+//  Created by Александр Х on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
